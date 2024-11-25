4661023a414d0586a20cd4d09f96610d376498c0
rizkyyfaauzi21 <muhammadrizkyfauzi061102@gmail.com>
2024-11-25 22:18:42 +0700

    W11: Soal 4,5,6

--- Jobsheet11/books/LaporanPraktikum.md
+++ Jobsheet11/books/LaporanPraktikum.md
@@ -10,7 +10,21 @@
 ![alt text](image.png)
 
 ### Soal 3
+![alt text](image-2.png)
 - substring(0, 450) mengambil potongan teks dari indeks ke-0 hingga ke-449.
 - .catchError((_) { ... }) digunakan untuk menangani error yang terjadi selama proses getData().        
 
-![alt text](image-2.png)
+## Praktikum 2
+### Soal 4
+![alt text](image-4.png)
+- Ketiga method (returnOneAsync, returnTwoAsync, dan returnThreeAsync) adalah fungsi asynchronous yang masing-masing mengembalikan nilai integer (1, 2, dan 3). Setelah delay selama 3 detik, masing-masing fungsi akan mengembalikan nilai integer (1, 2, atau 3).
+- Method count() adalah fungsi asynchronous yang digunakan untuk menghitung total dari hasil ketiga fungsi di atas.
+
+## Praktikum 3
+### Soal 5
+![alt text](image-5.png)
+- Kode ini menggunakan Completer untuk mengontrol kapan sebuah Future selesai. Fungsi getNumber() membuat Completer<int>, memanggil calculate(), dan mengembalikan Future yang belum selesai. Fungsi calculate() menunggu 5 detik dengan Future.delayed, lalu menyelesaikan Future tersebut dengan nilai 42 menggunakan completer.complete(42). Hasilnya, Future yang dikembalikan oleh getNumber() akan selesai dengan nilai 42 setelah 5 detik, memungkinkan kontrol manual atas penyelesaian Future tersebut.
+
+### Soal 6
+![alt text](image-6.png)
+- Perbedaan antara langkah 2 dan langkah 5-6 ada di Error Handler. Pada langkah 2, method calculate() hanya menyelesaikan Future dengan nilai 42 setelah 5 detik tanpa penanganan error. Sementara itu, pada langkah 5, calculate() menggunakan blok try-catch, sehingga jika terjadi error, completer.completeError({}) akan dipanggil untuk menandai Future sebagai gagal. Di langkah 6, onPressed() diperbarui untuk menangani keberhasilan dengan then() yang menampilkan hasil, atau catchError() untuk menampilkan pesan error, sehingga aplikasi dapat menampilkan respons yang sesuai tergantung pada apakah Future berhasil atau gagal.
--- Jobsheet11/books/lib/main.dart
+++ Jobsheet11/books/lib/main.dart
@@ -2,6 +2,7 @@ import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart';
 import 'package:http/http.dart' as http;
+import 'package:async/async.dart';
 
 void main() {
   runApp(const MyApp());
@@ -40,6 +41,55 @@ class _FuturePageState extends State<FuturePage> {
     return http.get(url);
   }
 
+  Future<int> returnOneAsync() async {
+    await Future.delayed(const Duration(seconds: 3));
+    return 1;
+  }
+
+  Future<int> returnTwoAsync() async {
+    await Future.delayed(const Duration(seconds: 3));
+    return 2;
+  }
+
+  Future<int> returnThreeAsync() async {
+    await Future.delayed(const Duration(seconds: 3));
+    return 3;
+  }
+
+  Future count() async {
+    int total = 0;
+    total = await returnOneAsync();
+    total += await returnTwoAsync();
+    total += await returnThreeAsync();
+    setState(() {
+      result = total.toString();
+    });
+  }
+
+  late Completer completer;
+
+  Future getNumber() {
+    completer = Completer<int>();
+    calculate();
+    return completer.future;
+  }
+
+  Future calculate() async {
+    await Future.delayed(const Duration(seconds: 5));
+    completer.complete(42);
+  }
+
+  Future calculate2() async {
+    try {
+      await new Future.delayed(const Duration(seconds: 5));
+      completer.complete(42);
+    } catch (_) {
+      completer.completeError({});
+    }
+    await Future.delayed(const Duration(seconds: 5));
+    completer.complete(42);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -52,15 +102,24 @@ class _FuturePageState extends State<FuturePage> {
           ElevatedButton(
             child: const Text('GO!'),
             onPressed: () {
-              setState(() {
-                getData().then((value) {
-                  result = value.body.toString().substring(0, 450);
-                  setState(() {});
-                }).catchError((_) {
-                  result = 'An error occurred';
-                  setState(() {});
+              getNumber().then((value) {
+                setState(() {
+                  result = value.toString();
                 });
+              }).catchError((e){
+                result = 'An error occurred';
               });
+
+              // count();
+              // setState(() {
+              //   getData().then((value) {
+              //     result = value.body.toString().substring(0, 450);
+              //     setState(() {});
+              //   }).catchError((_) {
+              //     result = 'An error occurred';
+              //     setState(() {});
+              //   });
+              // });
             },
           ),
           const Spacer(),
